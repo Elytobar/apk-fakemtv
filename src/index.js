01b2ffce0844474f435985981d1af6afaba03128
Ely Milena Tobar Montalvo <66325714+Elytobar@users.noreply.github.com>
2020-12-01 20:54:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+var port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log("JSON Server ejecutandose");
+});
